4ee0238f8daf692801c5a9445f23f38284cc565f
Mantas An <mantasantanavic@gmail.com>
2025-07-07 18:54:56 +0300

    pridetos iconos pakeistas base html visi html isskyrus pora,reikes sutavrkyt rytoj

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,8 +9,14 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=5000)
     created_at = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateField(null=True, blank=True)
+    due_date = models.DateTimeField(null=True, blank=True) # jeigu lyginsim po to ateityje svarbu DateTimeField ne DateField
     completed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
+
+    def is_overdue(self):
+        return self.due_date and self.due_date < timezone.now() and not self.completed
--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
-
+from django.utils import timezone
 from myapp.models import Task
 
 
@@ -14,24 +14,50 @@ class DashBoard(LoginRequiredMixin, ListView):
     template_name = 'tasks.html'
     context_object_name = 'tasks'
 
-class UpdateTask(LoginRequiredMixin, UpdateView):
+
+
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)   #si vieta rodo tik userio taskus
+
+class UpdateTask(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     model = Task
     template_name = 'update_task.html'
     fields = ['title', 'content', 'due_date', 'completed']
     success_url = reverse_lazy('tasks')
+
+    def test_func(self):
+        task = self.get_object()
+        return task.user == self.request.user
+
+    #-------------------BE SITO APACIOJ---------------------
+    #ISMES TIK 403 ERROR
+    # def handle_no_permission(self):
+    #     # Optional: Custom response if user fails test
+    #     from django.http import HttpResponseForbidden
+    #     return HttpResponseForbidden("🚫 Nope. This isn't your task to edit.")
+
 
 class DeleteTask(LoginRequiredMixin, DeleteView):
     model = Task
     template_name = 'delete_task.html'
     success_url = reverse_lazy('tasks')
 
+
+
 class CreateTask(LoginRequiredMixin, CreateView):
     model = Task
     template_name = 'create_task.html'
-    fields = ['user', 'title', 'content', 'due_date']
+    #fields = ['user', 'title', 'content', 'due_date']
+    fields = ['title', 'content', 'due_date']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user  #automatiskai padaro loged user kaip autoriu, ir istrinam is fields user
+        return super().form_valid(form)
 
 class DetailTask(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'detail_task.html'
     fields = ['user', 'title', 'content', 'created_at', 'due_date', 'completed']
+
